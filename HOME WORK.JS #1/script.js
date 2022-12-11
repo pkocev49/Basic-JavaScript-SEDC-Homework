ddc26f45ce9de5b2579f5135449d41458a748e7d
pkocev49 <120195818+pkocev49@users.noreply.github.com>
2022-12-11 18:42:11 +0100

    HOMEWOR #1

---
+++ HOME WORK.JS #1/script.js
@@ -0,0 +1,12 @@
+let onePhone = 119.95;
+
+let percentFromPhone = 5
+
+let result = (percentFromPhone / 100) * onePhone
+console.log(result)
+
+let finalPrice = onePhone - result
+console.log(finalPrice)
+
+let costOfAllPhones = finalPrice * 30
+console.log(costOfAllPhones)
