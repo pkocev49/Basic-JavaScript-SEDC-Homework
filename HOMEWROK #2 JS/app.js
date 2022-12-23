f0ca926fcebb3545888f86bb2a6758cbe01ceab3
pkocev49 <120195818+pkocev49@users.noreply.github.com>
2022-12-24 00:59:31 +0100

    homework2

---
+++ HOMEWROK #2 JS/app.js
@@ -0,0 +1,31 @@
+let year = parseInt(prompt("Enter a year"))
+year = (year - 4) % 12
+if (year === 0) {
+    console.log("RAT")
+} else if (year === 1) {
+    console.log("Ox")
+} else if (year === 2) {
+    console.log("Tiger")
+} else if (year === 3) {
+    console.log("Rabbit")
+} else if (year === 4) {
+    console.log("Dragon")
+} else if (year === 5) {
+    console.log("Snake")
+} else if (year === 6) {
+    console.log("Horse")
+} else if (year === 7) {
+    console.log("Goat")
+} else if (year === 8) {
+    console.log("Monkey")
+} else if (year === 9) {
+    console.log("Rooster")
+} else if (year === 10) {
+    console.log("dog")
+} else if (year === 11) {
+    console.log("Pig")
+}
+
+
+
+
